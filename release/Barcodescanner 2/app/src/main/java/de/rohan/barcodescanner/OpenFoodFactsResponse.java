415e01fef6f3483804842d94f4794f18dce26d33
DAA <rohanmuthukumar@Rohans-MacBook-Air.local>
2026-03-24 15:15:09 +0100

    Initial commit

---
+++ release/Barcodescanner 2/app/src/main/java/de/rohan/barcodescanner/OpenFoodFactsResponse.java
@@ -0,0 +1,8 @@
+package de.rohan.barcodescanner;
+
+public class OpenFoodFactsResponse {
+    public Product product;
+    public String status;
+
+
+}
---
+++ release/Barcodescanner 2/app/src/main/java/de/rohan/barcodescanner/Product.java
@@ -0,0 +1,9 @@
+package de.rohan.barcodescanner;
+
+public class Product {
+    public String code;
+    public String brands;
+    public String product_name;
+    public String image_url;
+
+}
